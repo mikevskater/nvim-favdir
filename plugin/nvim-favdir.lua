ceb1eedbb55243a1bcfc5eeab371a537b24a7341
mikevskater <mikevskater@hotmail.com>
2026-01-11 21:24:53 -0500

    fix: update require statements to match plugin name for user commands

--- plugin/nvim-favdir.lua
+++ plugin/nvim-favdir.lua
@@ -7,21 +7,21 @@ vim.g.loaded_favdir = true
 
 -- Create user commands
 vim.api.nvim_create_user_command('FavdirOpen', function()
-  require('favdir').show()
+  require('nvim-favdir').show()
 end, { desc = 'Open favorite directories UI' })
 
 vim.api.nvim_create_user_command('FavdirToggle', function()
-  require('favdir').toggle()
+  require('nvim-favdir').toggle()
 end, { desc = 'Toggle favorite directories UI' })
 
 vim.api.nvim_create_user_command('FavdirAddDir', function()
-  require('favdir').add_cwd()
+  require('nvim-favdir').add_cwd()
 end, { desc = 'Add current directory to favorites' })
 
 vim.api.nvim_create_user_command('FavdirAddFile', function()
-  require('favdir').add_file()
+  require('nvim-favdir').add_file()
 end, { desc = 'Add current file to favorites' })
 
 vim.api.nvim_create_user_command('FavdirSandbox', function()
-  require('favdir').show_sandbox()
+  require('nvim-favdir').show_sandbox()
 end, { desc = 'Open favorite directories UI in sandbox mode (no persistence)' })
